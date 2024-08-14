33c0c8a556d8d149e8ae4e03500f77d5e9c18b64
Haeil Lim <lhi7837@gmail.com>
2024-08-14 11:23:40 +0900

    각성하라

--- src/main/java/org/shds/company/CompanyApplication.java
+++ src/main/java/org/shds/company/CompanyApplication.java
@@ -2,7 +2,7 @@ package org.shds.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//조명근은 각성하라
 @SpringBootApplication
 public class CompanyApplication {
 
