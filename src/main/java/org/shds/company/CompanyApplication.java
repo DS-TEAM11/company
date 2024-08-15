0727d4c86685df94bca213e0d4e7a7a308df8ccb
Haeil Lim <lhi7837@gmail.com>
2024-08-15 21:19:05 +0900

    Security Config 이슈 해결 및 Spring AI 추가 #1

--- src/main/java/org/shds/company/CompanyApplication.java
+++ src/main/java/org/shds/company/CompanyApplication.java
@@ -2,10 +2,12 @@ package org.shds.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class CompanyApplication {
 
 	public static void main(String[] args) {
@@ -13,4 +15,3 @@ public class CompanyApplication {
 	}
 
 }
-//주석
