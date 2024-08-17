83bea50307a869b1fd85f8b4e6b66ce25926fd85
Haeil Lim <lhi7837@gmail.com>
2024-08-17 15:43:33 +0900

    undo: gpt code delete

--- src/main/java/org/shds/company/ai/Repository/BenefitLocationRepository.java
+++
@@ -1,9 +0,0 @@
-package org.shds.company.ai.Repository;
-import org.shds.company.ai.Entity.BenefitLocation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface BenefitLocationRepository extends JpaRepository<BenefitLocation, Integer> {
-    List<Integer> findByLocationNameContains(String name);
-}
--- src/main/java/org/shds/company/ai/Repository/CardRepository.java
+++
@@ -1,7 +0,0 @@
-package org.shds.company.ai.Repository;
-
-import org.shds.company.ai.Entity.Card;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CardRepository extends JpaRepository<Card, Integer> {
-}
